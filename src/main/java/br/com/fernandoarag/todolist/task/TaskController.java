84382af9c206fed73bfe6068bfd799344c5c964b
Fernando Malveira Aragão <fernandoarag@gmail.com>
2024-02-17 18:22:08 -0300

    Created tasks, filter and user controller

---
+++ src/main/java/br/com/fernandoarag/todolist/task/TaskController.java
@@ -0,0 +1,53 @@
+package br.com.fernandoarag.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        final Object IdUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) IdUser);
+
+        final LocalDateTime currentDAte = LocalDateTime.now();
+        if (currentDAte.isAfter(taskModel.getStartAt()) || currentDAte.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início/término deve ser maior que a data atual!");
+
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser menor que a data de término!");
+
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        final Object IdUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) IdUser);
+        return tasks;
+    }
+}
