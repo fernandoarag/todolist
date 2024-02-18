a6f94ba258a5992de6ade5e271f9b5e91a588aa3
fernandoarag <fernandoarag@gmail.com>
2024-02-18 16:47:05 -0300

    Implementado rota de deletar task

--- src/main/java/br/com/fernandoarag/todolist/task/TaskController.java
+++ src/main/java/br/com/fernandoarag/todolist/task/TaskController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -76,5 +77,27 @@ public class TaskController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(taskUpdated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElseThrow(null);
+        if (task == null) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa não encontrada!");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário não tem permissão para alterar essa tarefa!");
+        }
+
+        this.taskRepository.deleteById(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Tarefa excluida!");
     }
 };
